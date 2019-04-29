970df171663dc5f026f534cf70c45b383b69531c
Robin Dercole <robin.dercole@gmail.com>
2019-04-29 21:15:21 +0200

    more display

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
 	root 'gossip#index' 
-  get 'gossip/team'
-  get 'gossip/contact'
-  get 'gossip/contact'
-  get 'gossip/team'
+	get 'gossip/team'
+	get 'gossip/contact'
+	get 'gossip/contact'
+	get 'gossip/team'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'gossip/welcome/:first_name', to: 'gossip#welcome'
+  get 'welcome/:first_name', to: 'gossip#welcome'
 
   get '/gossip/:id', to:  'gossip#show_gossip', as:'gossip_truc'
- 	get '/user/:user', to: 'gossip#show_user', as: 'user_truc'
+  get '/user/:user', to: 'gossip#show_user', as: 'user_truc'
 
- end
+end
